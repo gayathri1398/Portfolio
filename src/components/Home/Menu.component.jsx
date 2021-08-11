166b3ee36c3f30384cd4936e7e3fc01c7d95beca
gayathri1398 <gayathrivasu13@gmail.com>
2021-08-12 01:58:14 +0530

    cleared the dropdown error

--- src/components/Home/Menu.component.jsx
+++ src/components/Home/Menu.component.jsx
@@ -1,27 +1,57 @@
 import { Menu } from '@headlessui/react';
+import {FiChevronDown} from 'react-icons/fi';
 
-export default function MyDropdown() {
+export default function MyDropdown(classname) {
   return (
-    <Menu >
-      <Menu.Button>Home</Menu.Button>
-      <Menu.Items>
-        <Menu.Item>
+    <Menu>
+      <Menu.Button>Home <FiChevronDown className="inline"/></Menu.Button>
+      <Menu.Items className="flex flex-col gap-2">
+        {/* Use the `active` render prop to conditionally style the active item. */}
+        <Menu.Item className="mt-2">
           {({ active }) => (
             <a
-              className={`${active && 'bg-blue-500'}`}
+              className={`${
+                active ? 'bg-blue-500 text-white' : 'bg-white text-black'
+              }`}
               href="/account-settings"
             >
-              About
+             About
             </a>
           )}
         </Menu.Item>
         <Menu.Item>
           {({ active }) => (
             <a
-              className={`${active && 'bg-blue-500'}`}
+              className={`${
+                active ? 'bg-blue-500 text-white' : 'bg-white text-black'
+              }`}
               href="/account-settings"
             >
-             Projects
+              Projects
+            </a>
+          )}
+        </Menu.Item>
+        <Menu.Item>
+          {({ active }) => (
+            <a
+              className={`${
+                active ? 'bg-blue-500 text-white' : 'bg-white text-black'
+              }`}
+              href="/account-settings"
+            >
+             Skills
+            </a>
+          )}
+        </Menu.Item>
+        <Menu.Item>
+          {({ active }) => (
+            <a
+              className={`${
+                active ? 'bg-blue-500 text-white' : 'bg-white text-black'
+              }`}
+              href="/account-settings"
+            >
+             Contact
             </a>
           )}
         </Menu.Item>
@@ -30,3 +60,36 @@ export default function MyDropdown() {
     </Menu>
   )
 }
+
+// import { Menu } from '@headlessui/react';
+
+// export default function MyDropdown() {
+//   return (
+//     <Menu >
+//       <Menu.Button>Home</Menu.Button>
+//       <Menu.Items>
+//         <Menu.Item>
+//           {({ active }) => (
+//             <a
+//               className={`${active && 'bg-blue-500'}`}
+//               href="/account-settings"
+//             >
+//               About
+//             </a>
+//           )}
+//         </Menu.Item>
+//         <Menu.Item>
+//           {({ active }) => (
+//             <a
+//               className={`${active && 'bg-blue-500'}`}
+//               href="/account-settings"
+//             >
+//              Projects
+//             </a>
+//           )}
+//         </Menu.Item>
+       
+//       </Menu.Items>
+//     </Menu>
+//   )
+// }
--- src/components/Home/Navbar.component.jsx
+++ src/components/Home/Navbar.component.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import {FiChevronDown} from 'react-icons/fi';
+
 import MyDropdown from './Menu.component';
 
 const Navsm=()=>{
    return(
    <>
-     <div className="flex gap-0 items-center lg:flex gap-6 justify-around border-2 bg-port-300">
+     <div className="flex gap-0 items-center justify-around border-2 bg-port-300">
        <div>
-          <h2 className="font-bold text-2xl italic lg:text-3xl">Gayathri</h2>
+          <h2 className="font-bold text-2xl italic md:text-3xl">Gayathri</h2>
           </div>
-          <div className="flex h-8 gap-0 lg:gap-20 lg:flex-row font-semibold text-2xl cursor-pointer">
-          {/* <p className="flex-col">  <MyDropdown/><FiChevronDown className="text-black inline  lg:hidden"/></p> */}
+          <div className="flex h-8 gap-0 font-normal text-xl cursor-pointer md:text-2xl">
           <p>  <MyDropdown /> </p>
          
           </div>
@@ -47,6 +46,7 @@ const Navsm=()=>{
     return<>
       <nav>
         <div className="lg:hidden">{/*mobile devices */}  <Navsm/> </div>
+       
         <div className="hidden lg:block">{/*large devices */}  <Navlg/> </div>
       </nav>
       </>
