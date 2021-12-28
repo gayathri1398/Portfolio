950312cb7e8d420be14eb3b30c5c3487e168f14d
gayathri1398 <gayathrivasu13@gmail.com>
2021-12-29 00:21:04 +0530

    added the profile

--- src/components/About/About.component.jsx
+++ src/components/About/About.component.jsx
@@ -4,6 +4,10 @@ import React from 'react'
     return (
         <div>
         <h1 className="h-12 text-2xl italic font-semibold bg-port-300 pl-4 pt-2 md:text-3xl md:pt-4 md:h-16 lg:h-20 lg:text-5xl">About</h1>
+         <p>
+             A B.Tech graduated student 
+         </p>
+
         </div>
     )
 }
--- src/components/Skills/Skills.component.jsx
+++ src/components/Skills/Skills.component.jsx
@@ -2,9 +2,50 @@ import React from 'react'
 
 const Skills = () => {
     return (
+        <>
         <div>
         <h1 className="h-12 text-2xl italic font-semibold bg-port-300 pl-4 pt-2 md:text-3xl md:pt-4 md:h-16 lg:h-20 lg:text-5xl">Skills</h1>
         </div>
+      <div className=" px-6 flex gap-12 justify-evenly text-center flex-wrap my-9 md:justify-between md:gap-28 lg:gap-48 lg:px-28">
+      <div className="w-20 h-20 md:w-24 h-24 lg:w-32 h-32 mb-2">
+            <img src="https://andrewborstein.com/assets/img/html5.png" alt=" "  />
+            <h2 className="mt-2 font-bold italic lg:text-xl">HTML</h2>
+        </div>
+        <div className="w-20 h-20 md:w-24 h-24 lg:w-32 h-32 mb-2 ">
+            <img src="https://andrewborstein.com/assets/img/css3.png" alt=" "  />
+            <h2 className="mt-2 font-bold italic lg:text-xl">CSS</h2>
+        </div>
+        <div className="w-20 h-20 md:w-24 h-24 lg:w-32 h-32 mb-2">
+            <img src="https://andrewborstein.com/assets/img/bootstrap.png" alt=" "  />
+            <h2 className="mt-2 font-bold italic lg:text-xl">Bootstrap</h2>
+        </div>
+        <div className="w-20 h-20 md:w-24 h-24 lg:w-32 h-32 mb-2">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRfUeCuu7761MEqzuB8k3FcvCQ0C3XO0nuZQ&usqp=CAU" alt=" "  />
+            <h2 className="mt-2 font-bold italic lg:text-xl">TailwindCSS</h2>
+        </div>
+        <div className="w-20 h-20 md:w-24 h-24 lg:w-32 h-32 mb-2">
+            <img src="https://andrewborstein.com/assets/img/react.png" alt=" "  />
+            <h2 className="mt-2 font-bold italic lg:text-xl">React</h2>
+        </div>
+        <div className="w-20 h-20 md:w-24 h-24 lg:w-32 h-32 mb-2">
+            <img src="https://andrewborstein.com/assets/img/javascript.png" alt=" "  />
+            <h2 className="mt-2 font-bold italic lg:text-xl">Javascript</h2>
+        </div>
+        <div className="w-20 h-20 md:w-24 h-24 lg:w-32 h-32 mb-2">
+            <img src="https://andrewborstein.com/assets/img/nodejs.png" alt=" "  />
+            <h2 className="mt-2 font-bold italic lg:text-xl">Nodejs</h2>
+        </div>
+        <div className="w-20 h-20 md:w-24 h-24 lg:w-32 h-32 mb-2">
+            <img src="https://andrewborstein.com/assets/img/mongodb.png" alt=" "  />
+            <h2 className="mt-2 font-bold italic lg:text-xl">MongoDB</h2>
+        </div>
+        <div className="w-20 h-20 md:w-24 h-24 lg:w-32 h-32 mb-2">
+            <img src="https://andrewborstein.com/assets/img/github.png" alt=" "  />
+            <h2 className="mt-2 font-bold italic lg:text-xl">Github</h2>
+        </div>
+       
+      </div>
+      </> 
     )
 }
 
